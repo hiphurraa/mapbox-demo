7077156607f181c0d99720ab8fd02fc1f3fdb5c9
Tomi Heikkala <61519166+hiphurraa@users.noreply.github.com>
2022-05-16 16:10:10 +0300

    Removed redundant root directory and edited sql file

--- mapmarker.sql
+++ mapmarker.sql
@@ -14,10 +14,4 @@ CREATE TABLE IF NOT EXISTS `mapmarker`.`marker` (
 ENGINE = InnoDB;
 
 INSERT INTO mapmarker.marker (latitude, longitude, title, description)
-VALUES ("62.896005", "27.681000", "Hurja Solutions Oy", "Hurjan hyvä ohjelmistoyritys!");
-
-INSERT INTO mapmarker.marker (latitude, longitude, title, description)
 VALUES ("62.895379", "27.657227", "Tomi Heikkala", "Hurjan motivoitunut työntekijä!");
-
-INSERT INTO mapmarker.marker (latitude, longitude, title, description)
-VALUES ("62.878554", "27.710746", "Esimerkkimerkki", "Tässäpä tämmönen esimerkkimerkki jossa on oikein pitkä kuvaus, jotta nähdään, miten sivusto händlää tämmöiset vähän pidemmät kuvaukset. Tietokannassa on kuvaukselle rajoitus 200 merkkiä.");
